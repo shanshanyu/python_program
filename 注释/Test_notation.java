045619ef4ed5ada0d05c5917dbb5417a4bbfb425
shanshanyu <1525996190.qq.com>
2023-08-31 20:40:01 +0800

    new file

---
+++ 注释/Test_notation.java
@@ -0,0 +1,20 @@
+
+
+// public class 表示公共类每个文件只能有一个       #单行注释
+public class Test_notation{
+
+  /*
+ *public static void main 是主函数，是程序的入口   #多行注释
+ */
+  public static void main(String []args){
+    System.out.println("hello world");
+  }
+}
+
+
+/**
+ *这是文档注释
+ *
+ *
+ *
+ */
