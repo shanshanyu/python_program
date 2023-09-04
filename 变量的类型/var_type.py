fd5cbb0e161145f5d87533b6052b76d8c517a276
shanshanyu <1525996190.qq.com>
2023-09-04 17:08:42 +0800

    new file

---
+++ 变量的类型/var_type.py
@@ -0,0 +1,30 @@
+a = 1
+b = 1.2
+c = 'abc'
+
+d = []
+e = ()
+f = {}
+
+g = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(type(d))
+print(type(e))
+print(type(f))
+
+print(type(g))
+
+'''
+输出如下：
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'list'>
+<class 'tuple'>
+<class 'dict'>
+<class 'bool'>
+'''
